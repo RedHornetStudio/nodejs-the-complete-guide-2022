027a12bec303fdbe4a3ae729a260f88c55f3ea40
RedHornetStudio <51362978+RedHornetStudio@users.noreply.github.com>
2022-03-07 16:42:57 +0200

    added process.env.PORT modified:   app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const port = 3000;
+const PORT = process.env.PORT || 5000;
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -17,6 +17,6 @@ app.use('/', (req, res, next) => {
   res.status(404).send('<h1>404 Page not found</h1>');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
 })
