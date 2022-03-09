41e7adb769108e8db1b405b53586b5f4df6e944e
RedHornetStudio <51362978+RedHornetStudio@users.noreply.github.com>
2022-03-09 12:27:48 +0200

    section 5.working_with_express.js complited new file:   Views/404.html modified:   Views/add-product.html modified:   Views/shop.html modified:   app.js modified:   package.json new file:   public/css/main.css new file:   public/css/product.css modified:   routes/admin.js modified:   routes/shop.js new file:   util/rootDir.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,12 @@
 const path = require('path');
-
 const express = require('express');
+
+const rootDir = require('../util/rootDir');
 
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-  console.log(path.join(__dirname, '..', 'Views', 'add-product.html'));
-  res.sendFile(path.join(__dirname, '..', 'Views', 'add-product.html'));
+  res.sendFile(path.join(rootDir, 'Views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,12 @@
 const path = require('path');
-
 const express = require('express');
+
+const rootDir = require('../util/rootDir');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '..', 'Views', 'shop.html'));
+  res.sendFile(path.join(rootDir, 'Views', 'shop.html'));
 });
 
 module.exports = router;
